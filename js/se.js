097022f65c9a61a4eac2dc079029e40aa8d2774b
Voisin de Berthe <davidbrandes0@gmail.com>
2023-11-14 16:40:41 +0100

    supp bootstap

--- js/se.js
+++ js/se.js
@@ -1,28 +1,14 @@
-Array.from(document.querySelectorAll('.tabs')).forEach((tab_container, TabID) => {
-    const registers = tab_container.querySelector('.tab-registers');
-    const bodies = tab_container.querySelector('.tab-bodies');
-  
-    Array.from(registers.children).forEach((el, i) => {
-      el.setAttribute('aria-controls', `${TabID}_${i}`)
-      bodies.children[i]?.setAttribute('id', `${TabID}_${i}`)
-    
-      el.addEventListener('click', (ev) => {
-        let activeRegister = registers.querySelector('.active-tab');
-        activeRegister.classList.remove('active-tab')
-        activeRegister = el;
-        activeRegister.classList.add('active-tab')
-        changeBody(registers, bodies, activeRegister)
-      })
-  })
-})
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-
-function changeBody(registers, bodies, activeRegister) {
-    Array.from(registers.children).forEach((el, i) => {
-        if (bodies.children[i]) {
-            bodies.children[i].style.display = el == activeRegister ? 'block' : 'none'
-        }
-
-        el.setAttribute('aria-expanded', el == activeRegister ? 'true' : 'false')
-    })
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
 }
