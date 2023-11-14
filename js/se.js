ac77340f5bca7c52bbec02ec7ef6c5670fc8d031
dbs78 <davidbrandes0@gmail.com>
2023-11-14 22:56:38 +0100

    commit

--- js/se.js
+++ js/se.js
@@ -1,28 +1,50 @@
+
+/***********************************************************************************************************
+Gestion des onglets
+*************************************************************************************************************/
 Array.from(document.querySelectorAll('.tabs')).forEach((tab_container, TabID) => {
-    const registers = tab_container.querySelector('.tab-registers');
-    const bodies = tab_container.querySelector('.tab-bodies');
+  const registers = tab_container.querySelector('.tab-registers');
+  const bodies = tab_container.querySelector('.tab-bodies');
+
+  Array.from(registers.children).forEach((el, i) => {
+    el.setAttribute('aria-controls', `${TabID}_${i}`)
+    bodies.children[i]?.setAttribute('id', `${TabID}_${i}`)
   
-    Array.from(registers.children).forEach((el, i) => {
-      el.setAttribute('aria-controls', `${TabID}_${i}`)
-      bodies.children[i]?.setAttribute('id', `${TabID}_${i}`)
-    
-      el.addEventListener('click', (ev) => {
-        let activeRegister = registers.querySelector('.active-tab');
-        activeRegister.classList.remove('active-tab')
-        activeRegister = el;
-        activeRegister.classList.add('active-tab')
-        changeBody(registers, bodies, activeRegister)
-      })
-  })
+    el.addEventListener('click', (ev) => {
+      let activeRegister = registers.querySelector('.active-tab');
+      activeRegister.classList.remove('active-tab')
+      activeRegister = el;
+      activeRegister.classList.add('active-tab')
+      changeBody(registers, bodies, activeRegister)
+    })
+})
 })
 
 
 function changeBody(registers, bodies, activeRegister) {
-    Array.from(registers.children).forEach((el, i) => {
-        if (bodies.children[i]) {
-            bodies.children[i].style.display = el == activeRegister ? 'block' : 'none'
-        }
+  Array.from(registers.children).forEach((el, i) => {
+      if (bodies.children[i]) {
+          bodies.children[i].style.display = el == activeRegister ? 'block' : 'none'
+      }
 
-        el.setAttribute('aria-expanded', el == activeRegister ? 'true' : 'false')
-    })
+      el.setAttribute('aria-expanded', el == activeRegister ? 'true' : 'false')
+  })
+}
+
+/***********************************************************************************************************
+gestion de l'accordeon panel
+*************************************************************************************************************/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
