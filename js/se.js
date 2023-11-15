6e3cb7f1788a340761b00f920273e77fb51ccdfe
dbs78 <davidbrandes0@gmail.com>
2023-11-15 23:41:45 +0100

    commit

--- js/se.js
+++ js/se.js
@@ -1,4 +1,56 @@
 /***********************************************************************************************************
+Variables contenant toutes les données de référence du jeu
+*************************************************************************************************************/
+const  technoData =[
+  {id: "SS1", tech: "SS", version: "1", libelle:"Ship Size", level : [1,0,2,10,3,15,4,20,5,20,6,20]},
+  {id: "A1", tech: "A", version: "1", libelle:"Attack", level : [0,0,1,20,2,30,3,25]},
+  {id: "D1", tech: "D", version: "1", libelle:"Defense", level : [0,0,1,20,2,30,3,25]},
+]
+ ;
+
+
+
+/***********************************************************************************************************
+Gestion de la BDD du navigateur
+*************************************************************************************************************/
+
+/* Initialisation de la base*/
+const dbName = "se";
+
+const request = indexedDB.open(dbName, 2);
+
+request.onerror = (event) => {
+  // Handle errors.
+};
+request.onupgradeneeded = (event) => {
+  const db = event.target.result;
+
+  // Create an objectStore to hold information about our customers. We're
+  // going to use "ssn" as our key path because it's guaranteed to be
+  // unique - or at least that's what I was told during the kickoff meeting.
+  const objectStore = db.createObjectStore("technologie", { keyPath: "id" });
+
+  // Use transaction oncomplete to make sure the objectStore creation is
+  // finished before adding data into it.
+  objectStore.transaction.oncomplete = (event) => {
+    // Store values in the newly created objectStore.
+    const technoObjectStore = db
+      .transaction("technologie", "readwrite")
+      .objectStore("technologie");
+    technoData.forEach((techno) => {
+      technoObjectStore.add(techno);
+    });
+  };
+};
+
+
+
+
+
+
+
+
+/***********************************************************************************************************
 Gestion des onglets
 *************************************************************************************************************/
 Array.from(document.querySelectorAll('.tabs')).forEach((tab_container, TabID) => {
